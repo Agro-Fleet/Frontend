093ae6a6ce90eb861a3e35f9c3826307de15b432
iago <iagokater@live.com>
2024-04-23 15:56:33 -0300

    feat: static login screen

--- agro-fleet/src/app/page.tsx
+++ agro-fleet/src/app/page.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import Home from './pages/home/home';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import Login from './pages/login/login';
 
 
 const Page: React.FC = () => {
   return (
     <div>
-      <Home />
-      {/* <Login /> */}
+      {/* <Home /> */}
+      <Login />
     </div>
   );
 };
--- agro-fleet/src/app/pages/login/login.tsx
+++ agro-fleet/src/app/pages/login/login.tsx
@@ -1,10 +1,17 @@
+'use client'
 import React from "react";
 import "./login.scss";
 import login from "../../../../public/login.svg";
 import Image from "next/image";
-import { InputText } from "primereact/inputtext";
+import { useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="screen">
       <div className="img">
@@ -14,7 +21,18 @@ function Login() {
       <div className="form">
         <div className="login">
           <h1>Login</h1>
-          
+          <span className="p-float-label">
+            <InputText id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <label htmlFor="username">Email</label>
+          </span>
+          <span className="p-float-label">
+            <Password id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <label htmlFor="password">Senha</label>
+          </span>
+          <Button className="entrar">Entrar</Button>
+          <Button className="sair">Sair</Button>
+          {/* link redefinir senha */}
+          <a href="#">Redefinir senha</a>
         </div>
       </div>
     </div>
